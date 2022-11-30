af04131c89f7b031acb5456414aead6bc9aea247
dwest77a <daniel.westwood@stfc.ac.uk>
2022-11-30 14:53:54 +0000

    Fixed tests

--- tests/test_datasetMath.py
+++ tests/test_datasetMath.py
@@ -1,13 +1,13 @@
 import pytest
 
 def test_sigma(dataset1d, tsigma):
-    from pyanalysis.pyanalysis.datasetMath import sigma
-    assert sigma(dataset1d) == tsigma
+    from pyanalysis.datasetMath import sigma
+    assert (sigma(dataset1d) - tsigma) < 0.001
 
 
 def test_variance(dataset1d, tvariance):
-    from pyanalysis.pyanalysis.datasetMath import variance
-    assert variance(dataset1d) == tvariance
+    from pyanalysis.datasetMath import variance
+    assert (variance(dataset1d) - tvariance) < 0.001 
 
 def test_std_error():
     assert True
